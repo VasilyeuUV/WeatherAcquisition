2c807e108353770cf457d144838d9f9578beba0a
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-06 09:54:17 +0300

    dal: базовая структура с пакетами

---
+++ Data/WeatherAcquisition.DAL/Contexts/DataBaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherAcquisition.DAL.Contexts
+{
+    internal class DataBaseContext : DbContext
+    {
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="options"></param>
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+    }
+}
