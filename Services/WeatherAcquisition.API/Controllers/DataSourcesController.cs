9c5f6c5a39fb92bd26b07f571805c928ec75ef56
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-08 07:13:25 +0300

    api: WebApi для доступа к репозиторию источников данных

---
+++ Services/WeatherAcquisition.API/Controllers/DataSourcesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WeatherAcquisition.DAL.Entities;
+using WeatherAcquisition.Interfaces.Base.Repositories;
+
+namespace WeatherAcquisition.API.Controllers
+{
+    /// <summary>
+    /// Контроллер, который будет управлять DataSource
+    /// </summary>
+    [Route("api/[controller]")] // - адрес по умолчанию
+    [ApiController]
+    public class DataSourcesController : ControllerBase
+    {
+        private readonly IRepository<DataSource> _repository;
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="repository">Репозиторий, к которому будет осуществлять доступ контроллер</param>
+        public DataSourcesController(IRepository<DataSource> repository) 
+            => this._repository = repository;
+
+
+        /// <summary>
+        /// Получение полного количества записей
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("count")] // - указывает, как этот метод будет выглядеть со стороны WebAPI (т.е. как обратиться к нему)
+                           // типа api/count
+        public async Task<IActionResult> GetItemsCount()
+            => Ok(await _repository.GetCountAsync());
+    }
+}
