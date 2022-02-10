fdbf885b69a6a8d5309d40bf137bf69a9f7dfbc7
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-10 16:38:55 +0300

    api: Контроллер под DataValue

---
+++ Services/WeatherAcquisition.API/Controllers/DataValueController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAcquisition.API.Controllers._Base;
+using WeatherAcquisition.DAL.Entities;
+using WeatherAcquisition.Interfaces.Base.Repositories;
+
+namespace WeatherAcquisition.API.Controllers
+{
+    /// <summary>
+    /// Контроллер, который будет управлять DataValue
+    /// </summary>
+    [ApiController]
+    public class DataValueController : AEntityControllerBase<DataValue>
+    {
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="repository"></param>
+        public DataValueController(IRepository<DataValue> repository) : base(repository) { }
+
+    }
+}
