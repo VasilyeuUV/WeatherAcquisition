c88eac10ce33c964062817702f00264cf6c3fff1
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-15 21:15:14 +0300

    api: Контроллер репозитория источника данных

---
+++ Services/WeatherAcquisition.API/Controllers/DataSourceRepositoryController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using WeatherAcquisition.API.Controllers._Base;
+using WeatherAcquisition.DAL.Entities;
+using WeatherAcquisition.Domain.Base.Models;
+using WeatherAcquisition.Interfaces.Base.Repositories;
+
+namespace WeatherAcquisition.API.Controllers
+{
+
+    /// <summary>
+    /// Контроллер для управления источниками данных 
+    /// </summary>
+    // адрес указан в базовом классе
+    public class DataSourceRepositoryController : AMappedEntityController<DataSourceDomainBaseModel, DataSource>
+    {
+
+        /// <summary>
+        /// CTOR
+        /// </summary>
+        /// <param name="repository"></param>
+        /// <param name="mapper"></param>
+        public DataSourceRepositoryController(IRepository<DataSource> repository, IMapper mapper)
+            : base(repository, mapper) { }
+    }
+}
