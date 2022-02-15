b98e39f124c4bc0f4f28da1b859acfc0c5917165
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-15 19:49:24 +0300

    domain: Модели данных бизнес-логики

---
+++ Common/WeatherAcquisition.Domain.Base/Models/DataSourceDomainBaseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WeatherAcquisition.Interfaces.Base.Entities;
+
+namespace WeatherAcquisition.Domain.Base.Models
+{
+    /// <summary>
+    /// Доменная модель источников данных
+    /// </summary>
+    public class DataSourceDomainBaseModel : INamedEntity
+    {
+        /// <summary>
+        /// Описание модели
+        /// </summary>
+        public string Description { get; set; }
+
+        //########################################################################
+        #region INamedEntity
+
+        public Guid Id { get; }
+
+        public string Name { get; set; }
+
+        #endregion // INamedEntity
+
+    }
+}
---
+++ Common/WeatherAcquisition.Domain.Base/Models/DataValueDomainBaseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using WeatherAcquisition.Interfaces.Base.Entities;
+
+namespace WeatherAcquisition.Domain.Base.Models
+{
+    /// <summary>
+    /// Доменная модель данных
+    /// </summary>
+    public class DataValueDomainBaseModel : IEntity
+    {
+
+        public DateTimeOffset Dtg { get; set; }
+        public string Value { get; set; }
+        public bool IsFault { get; set; }
+
+        //##############################################################################
+        #region IEntity
+
+        public Guid Id { get; }
+
+        #endregion // IEntity
+
+    }
+}
