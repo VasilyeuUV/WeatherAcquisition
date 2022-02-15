567f250b2369d476c5810a5156bdb881887247ad
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-15 15:01:19 +0300

    blazor: Настройка ползовательских библиотек web-интерфейса Blazor

--- UI/WeatherAcquisition.BlazorUI/Program.cs
+++ UI/WeatherAcquisition.BlazorUI/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WeatherAcquisition.BlazorUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app");    // #app - идентификатор блока из index.html, где будет отображаться приложение
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
