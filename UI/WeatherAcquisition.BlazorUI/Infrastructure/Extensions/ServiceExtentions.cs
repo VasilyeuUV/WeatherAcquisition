6619634578edb9db923e2249f9d584d2200bf3be
VasilyeuUV <vasilyeu.uv@mail.ru>
2022-02-15 21:48:26 +0300

    blazor: Регистрация контроллера репозитория в клиенте

---
+++ UI/WeatherAcquisition.BlazorUI/Infrastructure/Extensions/ServiceExtentions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace WeatherAcquisition.BlazorUI.Infrastructure.Extensions
+{
+
+    /// <summary>
+    /// Расширения сервисов
+    /// </summary>
+    internal static class ServiceExtentions
+    {
+        /// <summary>
+        /// Метод расширения для регистрации контроллеров в приложении 
+        /// </summary>
+        /// <typeparam name="IInterface"></typeparam>
+        /// <typeparam name="IClient"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="adress"></param>
+        /// <returns></returns>
+        public static IHttpClientBuilder AddApi<IInterface, IClient>(this IServiceCollection services, string adress) 
+            where IInterface : class where IClient : class, IInterface
+            => services
+            .AddHttpClient<IInterface, IClient>(
+                // Конфигурируем HttpClient (меняем у объекта клиента базовый адрес):
+                // - базовый адрес берем из хоста;
+                // - добавляем к нему путь Контроллера репозитория источников данных
+                (host, client) => client.BaseAddress = new($"{host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress}{adress}")
+                );
+    }
+}
--- UI/WeatherAcquisition.BlazorUI/Program.cs
+++ UI/WeatherAcquisition.BlazorUI/Program.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WeatherAcquisition.BlazorUI;
+using WeatherAcquisition.BlazorUI.Infrastructure.Extensions;
+using WeatherAcquisition.Domain.Base.Models;
+using WeatherAcquisition.Interfaces.Base.Repositories;
+using WeatherAcquisition.WebAPIClients.Clients;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");    // #app - идентификатор блока из index.html, где будет отображатьс€ приложение
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var services = builder.Services;
+services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Ёто повтор€ем дл€ всех репозиториев, которые будут в приложении
+//services.AddHttpClient<IRepository<DataSourceDomainBaseModel>, WebRepositoryClient<DataSourceDomainBaseModel>>(
+//    //  онфигурируем HttpClient (мен€ем у объекта клиента базовый адрес):
+//    // - базовый адрес берем из хоста;
+//    // - добавл€ем к нему путь  онтроллера репозитори€ источников данных
+//    (host, client) => client.BaseAddress = new(host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress + "api/DataSourceRepository")  
+//    );
+
+// ”прощение регистрации контроллеров репозиториев
+services.AddApi<IRepository<DataSourceDomainBaseModel>, WebRepositoryClient<DataSourceDomainBaseModel>>("api/DataSourceRepository");
 
 await builder.Build().RunAsync();
